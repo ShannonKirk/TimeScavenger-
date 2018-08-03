551e32728816eef770a98fc0ec79a171a2c7cf1d
Unknown <shannonkirk1999@gmail.com>
2018-08-03 13:53:14 +1200

    Player Flip

--- TimeScavengerProject/Assets/Scripts/Grabber.cs
+++ TimeScavengerProject/Assets/Scripts/Grabber.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Grabber : MonoBehaviour {
 
+    public bool grabbed;
+    RaycastHit2D hit;
+    public float distance = 2f;
+    public Transform holdpoint;
+    public float throwforce;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +17,37 @@ public class Grabber : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            if(!grabbed)
+            {
+                Physics2D.queriesStartInColliders = false;
+            
+
+               hit =  Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x,distance);
+
+                if (hit.collider != null)
+                {
+                    grabbed = true;
+                }
+
+            }
+
+            else
+            {
+                grabbed = false;
+
+                if (hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+                {
+                    hit.collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x, 1) * throwforce;
+                }
+            }
+        }
+
+        if (grabbed)
+            hit.collider.gameObject.transform.position = holdpoint.position;
+        
 		
 	}
 }
